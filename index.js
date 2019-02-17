68e183982f544fb5768e7813f53523b27ec01e5f
yukie7 <lenovo690@yahoo.co.jp>
2019-02-17 21:44:42 +0900

    コメント追記

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Pythonのimportに相当
 const line = require('@line/bot-sdk');
 const express = require('express');
 
@@ -12,16 +13,27 @@ const config = {
 // create LINE SDK client
 const client = new line.Client(config);
 
+// サーバーサイドの実行環境を提供するAPI
+// WEBサーバーを立てるためのAPI
+// サーバー上でWEBアプリを呼び出すためのもの
 // create Express app
 // about Express itself: https://expressjs.com/
 const app = express();
 
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
+// LINEに"あ"を送信：req <- "あ"
 app.post('/callback', line.middleware(config), (req, res) => {
-    Promise
+    // LINEで"あ" → "い"の順に送信　→　"あ" → "い"の順に返信
+    // 同期処理の場合，"い" → "あ"の順に返信される可能性アリ
+    Promise // 非同期処理
+        // all : とりあえずこいつを初めに実行
+        // 本体の処理はhandleEventの中
         .all(req.body.events.map(handleEvent))
-        .then((result) => res.json(result))
+        // then : allで成功した時，帰ってくるresultに対して～
+        // (result) = handleEventのreturnで帰ってくる値
+        .then((result) => res.json(result)) // json形式
+        // then : allで失敗した時，帰ってくるerrに対して～
         .catch((err) => {
             console.error(err);
             res.status(500).end();
@@ -30,15 +42,20 @@ app.post('/callback', line.middleware(config), (req, res) => {
 
 // event handler
 function handleEvent(event) {
+    // LINEがスタンプなど文字列以外の場合，無視
     if (event.type !== 'message' || event.message.type !== 'text') {
         // ignore non-text-message event
         return Promise.resolve(null);
     }
 
     // create a echoing text message
+    // event.message.text には"あ"が入っている
+    // これがjson形式{ field1 : value1, field2 : value2, ...}
     const echo = { type: 'text', text: event.message.text };
 
     // use reply API
+    // Promise Objec型をリターン（この中には，"あ"も含まれている状態）
+    // この時点でLINEに"あ"を返信
     return client.replyMessage(event.replyToken, echo);
 }
 
